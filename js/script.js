1c715db4be0bb4157ba22808d020c17bac947491
Meet Dholariya <129361022+dholariyameet@users.noreply.github.com>
2023-05-08 16:11:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(window).scroll(function(){
+    var sticky = $('.sticky'),
+        scroll = $(window).scrollTop();  
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+  });
